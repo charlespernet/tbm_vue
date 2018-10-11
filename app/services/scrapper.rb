d49c77766d4044fc4e189a485206186e6bc94fa3
Charles Pernet <charlespernet@gmail.com>
2018-10-11 14:17:18 +0200

    Add #data_for_line to Scrapper

--- app/services/scrapper.rb
+++ app/services/scrapper.rb
@@ -1,17 +1,26 @@
 require 'nokogiri'
 
 class Scrapper
+  def data_for_line(line)
+    # lianes 1 = 1 / Lianes 5 = 6
+    @data ||= process
+    @data.select { |bus| bus[:line] == line }
+  end
+
+  private
+
   def process
     file      = File.open('storage/tbm_example.xml')
     document  = Nokogiri::XML(file)
 
-    document.xpath('//bm:SV_ARRET_P').each do |beer|
-      name        = beer.xpath('bm:LIBELLE').text
-      theoric  = beer.xpath('bm:HOR_THEO').text
-      app      = beer.xpath('bm:HOR_APP').text
-      real      = beer.xpath('bm:HOR_REAL').text
-      puts "#{name} #{theoric} #{app} #{real}"
+    document.xpath('//bm:SV_ARRET_P').map do |stop|
+      {
+        line: stop.xpath('bm:RS_SV_LIGNE_A').text,
+        name: stop.xpath('bm:LIBELLE').text,
+        theoric: stop.xpath('bm:HOR_THEO').text,
+        app: stop.xpath('bm:HOR_APP').text,
+        real: stop.xpath('bm:HOR_REAL').text
+      }
     end
   end
-
 end
